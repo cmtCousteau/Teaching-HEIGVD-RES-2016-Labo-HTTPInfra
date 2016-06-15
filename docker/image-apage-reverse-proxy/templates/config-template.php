450bd1fd6a4047922069d01b90adfe1e48d9d8ef
CmtCousteau <marco.mon@windowslive.com>
2016-06-15 10:06:17 +0200

    dernière étape

---
+++ docker/image-apage-reverse-proxy/templates/config-template.php
@@ -0,0 +1,16 @@
+<?php
+	$static_app = getenv('STATIC_APP');
+	$dynamic_app = getenv('DYNAMIC_APP');
+?>
+<VirtualHost *:80>
+	ServerName demo.res.ch
+
+	ProxyPass '/api/locations/' 'http://<?php print "$dynamic_app"?>/'
+	ProxyPassReverse '/api/locations/' 'http://<?php print "$dynamic_app"?>/'
+	
+	ProxyPass '/' 'http://<?php print "$static_app"?>/'
+	ProxyPassReverse '/' '<?php print "$static_app"?>/'
+	
+</VirtualHost>
+HEADER
+FOOTER
